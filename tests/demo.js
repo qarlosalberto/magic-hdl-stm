338cfa148e46dfa7eafd09d17ee1c03d755c266d
Carlos Alberto <carlos.ruiz@softwareradiosystems.com>
2020-11-08 20:25:10 +0100

    up

--- tests/demo.js
+++ tests/demo.js
@@ -47,24 +47,24 @@ stm.push(state_2);
 
 let table = document.getElementById("table");
 
-add_stm(stm);
+add_stm_table(stm);
 
 
-function add_stm(stm) {
+function add_stm_table(stm) {
   for (let i = 0; i < stm.length; ++i) {
-    add_state(stm[i]);
+    add_state_table(stm[i]);
   }
 
 }
 
-function add_state(state) {
-  add_state_name(state.name);
+function add_state_table(state) {
+  add_state_name_table(state.name);
   for (let i = 0; i < state.transitions.length; ++i) {
-    add_state_transition(state.transitions[i]);
+    add_state_transition_table(state.transitions[i]);
   }
 }
 
-function add_state_name(name) {
+function add_state_name_table(name) {
   let row = table.insertRow(0);
   row.style.backgroundColor = '#ffd78c';
   let cell = row.insertCell(0);
@@ -72,7 +72,7 @@ function add_state_name(name) {
   cell.colSpan = '2';
 }
 
-function add_state_transition(transition) {
+function add_state_transition_table(transition) {
   let row = table.insertRow(0);
   row.style.backgroundColor = 'grey';
   let cell_destination = row.insertCell();
@@ -82,4 +82,17 @@ function add_state_transition(transition) {
   cell_condition.innerHTML = transition.condition;
   cell_condition.style.width = '50%';
 }
+
+oncontextmenu = (e) => {
+  e.preventDefault();
+  let menu = document.createElement("div");
+  menu.id = "ctxmenu";
+  menu.style = `top:${e.pageY}px;left:${e.pageX}px`;
+  menu.onmouseleave = function () { ctxmenu.outerHTML = ''; };
+  menu.innerHTML = "<p>Option1</p><p>Option2</p><p>Option3</p><p>Option4</p><p onclick='alert(`Thank you!`)'>Upvote</p>";
+  document.body.appendChild(menu);
+};
+
+
+
 
